f10dec36262e427598bf73d0754e453f6bbb561b
Mantas Geidrichis <mantasgeidrichis@Mantass-MacBook-Air.local>
2022-03-03 23:21:43 +0200

    testing card heigh

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
   return (
     <div className='tc bg-light-green dib br3 pa3 ma2 grow br2 shadow-5'>
-      <img alt='Robots' src={`https://robohash.org/set_set3/bgset_bg1/${id}/size=200x200`} />
+      <img alt='Robots' src={`https://robohash.org/set_set3/bgset_bg1/${id}/size=100x100`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
